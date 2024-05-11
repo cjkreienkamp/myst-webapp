1562c963d5005af0601d5fde4ed606e270b8cf4a
Chris Kreienkamp <55727671+cjkreienkamp@users.noreply.github.com>
2024-05-11 13:34:33 +0200

    adding extra files again
    Trying to get this to work on GitHub pages

---
+++ CMakeFiles/d/1bb779afb60a8ee9ebc1b16b9db03f80e630a52f819099a676d6c79930b393ac.d
@@ -0,0 +1,9 @@
+content/contentplugin_init_autogen/timestamp: \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Core/Qt6CoreConfigureFileTemplate.in \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Core/Qt6CoreResourceInit.in.cpp \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6QmlModuleDirMappingTemplate.qrc.in \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6QmlPluginTemplate.cpp.in \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6qmldirTemplate.cmake.in \
+  /Users/chriskreienkamp/code/qtprojects/Myst/content/CMakeLists.txt \
+  content/contentplugin_init.cpp \
+  /usr/local/Cellar/cmake/3.25.1/bin/cmake
---
+++ CMakeFiles/d/cb835980d905b79fa5d7b28b004c77fd5a0e3ff17f0d900be7fe53e910ba5bba.d
@@ -0,0 +1,8 @@
+_deps/ds-build/src/imports/logichelper/QuickStudioLogicHelper_autogen/timestamp: \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Core/Qt6CoreConfigureFileTemplate.in \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Core/Qt6CoreResourceInit.in.cpp \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6QmlModuleDirMappingTemplate.qrc.in \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6QmlPluginTemplate.cpp.in \
+  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6qmldirTemplate.cmake.in \
+  _deps/ds-src/src/imports/logichelper/CMakeLists.txt \
+  /usr/local/Cellar/cmake/3.25.1/bin/cmake
