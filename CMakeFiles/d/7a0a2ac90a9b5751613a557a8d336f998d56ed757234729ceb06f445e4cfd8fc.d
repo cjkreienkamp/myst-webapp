019ea83e3283e91fea43b13f3c7ee35055dd0f22
Chris Kreienkamp <55727671+cjkreienkamp@users.noreply.github.com>
2024-05-11 13:26:09 +0200

    deleting extra files
    Trying to get GitHub pages to host the code

--- CMakeFiles/d/7a0a2ac90a9b5751613a557a8d336f998d56ed757234729ceb06f445e4cfd8fc.d
+++
@@ -1,8 +0,0 @@
-_deps/ds-build/src/imports/effects_qt6/QuickStudioEffects_autogen/timestamp: \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Core/Qt6CoreConfigureFileTemplate.in \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Core/Qt6CoreResourceInit.in.cpp \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6QmlModuleDirMappingTemplate.qrc.in \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6QmlPluginTemplate.cpp.in \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6qmldirTemplate.cmake.in \
-  _deps/ds-src/src/imports/effects_qt6/CMakeLists.txt \
-  /usr/local/Cellar/cmake/3.25.1/bin/cmake
--- CMakeFiles/d/a02572b5063a371b689306a287b7240916c122ba66b2268f1481b89bd2b4a340.d
+++
@@ -1,8 +0,0 @@
-imports/Myst/Myst_autogen/timestamp: \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Core/Qt6CoreConfigureFileTemplate.in \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Core/Qt6CoreResourceInit.in.cpp \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6QmlModuleDirMappingTemplate.qrc.in \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6QmlPluginTemplate.cpp.in \
-  /Users/chriskreienkamp/Qt/6.7.0/wasm_singlethread/lib/cmake/Qt6Qml/Qt6qmldirTemplate.cmake.in \
-  /Users/chriskreienkamp/code/qtprojects/Myst/imports/Myst/CMakeLists.txt \
-  /usr/local/Cellar/cmake/3.25.1/bin/cmake
